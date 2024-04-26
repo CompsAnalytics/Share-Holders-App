1b2c69b4647c36eb7aa9ff36928aa304ddf8b572
Mary Mbugua <mbuguamels@gmail.com>
2024-04-26 16:07:28 +0300

    created the notes file

--- src/Components/Notes/Notes.js
+++ src/Components/Notes/Notes.js
@@ -5,14 +5,34 @@ function Notes() {
     <div>
        <form>
 
-       <ol>
-<li>Enter your ID No. to access your Sacco information. You will be able to check your Share Statement,Dividend Statement,Guarantorship,Loan Statement, Apply an instant Loan and post a question or a suggestion.</li>
-<li>If unable to access , Please liaise with Dan on 0785278786 or email: sacco@metro-hospital.com.</li>
-<li>Dividends applies to LAST Year's shares and NOT the current. </li>
-<li>WHT only applies to those who will tick to be paid their dividends , it does NOT apply to all the other modes.</li>
-<li>Please confirm your mobile Number since the dividends will be paid through the number indicated.</li>
-      </ol>
+       
+
+     
         </form> 
+
+        <table>
+          <tbody>
+          <tr >
+                  <th className='half' >1.Enter your ID No. to access your Sacco information. You will be able to check your Share Statement,Dividend Statement,Guarantorship,Loan Statement.Apply an intant Loan and post a question or a suggestion.</th>
+                  </tr>
+              <tr >
+                  <th className='half' >2.If unable to access , Please liaise with Dan on 0785278786 or email: sacco@metro-hospital.com.</th>
+                  </tr>
+                  <tr>
+                  <th className='half'>3.Dividends applies to LAST Year's shares and NOT the current. </th>
+                  </tr>
+                  <tr>
+                  <th>4.WHT only applies to those who will tick to be paid their dividends , it does NOT apply to all the other modes.</th>
+                  </tr>
+                  <tr>
+                  <th>5.Please confirm your mobile Number since the dividends will be paid through the number indicated.</th>
+                  </tr>
+                 
+                  </tbody>
+                  </table>
+                  
+                     
+        
     </div>
  )
 }
