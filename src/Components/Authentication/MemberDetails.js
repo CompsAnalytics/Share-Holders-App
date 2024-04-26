76175f2a8db6ab0b9fbf3ef945b048ed9fa04cd8
Mary Mbugua <mbuguamels@gmail.com>
2024-04-26 16:05:55 +0300

    added member route

--- src/Components/Authentication/MemberDetails.js
+++ src/Components/Authentication/MemberDetails.js
@@ -1,26 +1,100 @@
 import React, { useEffect, useState } from 'react'
-
+import axios from 'axios'
+const memberno =localStorage.getItem("member");
+const apiUrl='http://localhost:8080/api' ;
+const accessToken =localStorage.getItem("access");
+console.log("ACCESS TOKEN FROM LOCAL STORAGE ", accessToken)
+const authAxios =axios.create({
+  // baseUrl:'http://localhost:8080/api',
+  headers: {
+    Authorization:`Bearer ${accessToken}`,
+    
+  }
+})
 const MemberDetails = () => {
     const [member,setMember] = useState([])
-
+    const [totalSavings,setTotalSavings] = useState([])
+    const [totalDividendPayable,setTotalDividendPayable] = useState([])
+    const [totalShareCapital,setTotalShareCapital] = useState([])
     useEffect(()=> {
-        fetch("http://localhost:8001/members/1")
-        .then((response) => {
-            if (response.ok) {
-              response.json().then((member) => setMember(member));
-            }
-          });
-        },
-    [])
+      authAxios.get(`${apiUrl}/v1/member/${memberno}`)
+      .then((res) => {
+        console.log('res member ', res.data)
+        // if (res.ok) {
+        setMember(res.data);
+        }).catch(error =>{
+          console.log(error);
+        });
+      },
+  [])
+  useEffect(()=> {
+    authAxios.get(`${apiUrl}/v1/savings/sumTotal/${memberno}`)
+    .then((res) => {
+      console.log('res member ', res.data)
+      // if (res.ok) {
+      setTotalSavings(res.data);
+      }).catch(error =>{
+        console.log(error);
+      });
+    },
+[])
 
+useEffect(()=> {
+  authAxios.get(`${apiUrl}/v1/dividendPayable/sumTotal/${memberno}`)
+  .then((res) => {
+    console.log('res member ', res.data)
+    // if (res.ok) {
+      setTotalDividendPayable(res.data);
+    }).catch(error =>{
+      console.log(error);
+    });
+  },
+[])
+
+useEffect(()=> {
+  authAxios.get(`${apiUrl}/v1/shareCapital/sumTotal/${memberno}`)
+  .then((res) => {
+    console.log('res member ', res.data)
+    // if (res.ok) {
+      setTotalShareCapital(res.data);
+    }).catch(error =>{
+      console.log(error);
+    });
+  },
+[])
   return (
     <div>
-        <h3>Welcome to Metropolitan Sacco, {member.member} </h3>
-        <h4>Member Name: {member.member}</h4>
-        <h4>Member No: {member.id}</h4>
-        <h4>Tel:  {member.mobile}</h4>
-        <h4>Email: {member.email}</h4>
-        <h4>Savings: {member.savings}</h4>
+      <table>
+          <tbody>
+          <tr >
+                  <th className='half' >Welcome to Metropolitan Sacco App,{member.holdersName}</th>
+                  </tr>
+              <tr >
+                  <th className='half' >Member No:{member.accNo}</th>
+                  </tr>
+                  <tr>
+                  <th className='half'>Mobile No:{member.tel1}</th>
+                  </tr>
+                  <tr>
+                  <th>Email:{member.emailAdd}</th>
+                  </tr>
+                  <tr>
+                  <th>Id no:{member.idNo}</th>
+                  </tr>
+                  <tr>
+                  <th> Savings:{totalSavings}</th>
+                  </tr>
+                  <tr>
+                  <th> Share Capital:{totalShareCapital}</th>
+                  </tr>
+                  <tr>
+                  <th> Dividend Payable:{totalDividendPayable}</th>
+                  </tr>
+                  </tbody>
+                  </table>
+                  
+                     
+        
 
     </div>
   )
